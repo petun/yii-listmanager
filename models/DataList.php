598db1bd6498375ae751347938069af0680c550c
petun <petun911@gmail.com>
2014-04-10 01:04:17 +0400

    add list functionality

--- models/DataList.php
+++ models/DataList.php
@@ -11,6 +11,12 @@
  */
 class DataList extends ActiveRecord
 {
+
+	/* for giix improvement*/
+	public function label($count) {
+		return "Списки";
+	}
+
 	/**
 	 * @return string the associated database table name
 	 */
@@ -103,5 +109,34 @@ class DataList extends ActiveRecord
 
 	public function types() {
 		return Yii::app()->controller->module->lists;
+	}
+
+	/* List functionality */
+	private static $_items=array();
+
+	public static function items($type)
+	{
+		if(!isset(self::$_items[$type]))
+			self::loadItems($type);
+		return self::$_items[$type];
+	}
+
+	public static function item($type,$code)
+	{
+		if(!isset(self::$_items[$type]))
+			self::loadItems($type);
+		return isset(self::$_items[$type][$code]) ? self::$_items[$type][$code] : false;
+	}
+
+	private static function loadItems($type)
+	{
+		self::$_items[$type]=array();
+		$models=self::model()->findAll(array(
+				'condition'=>'type=:type',
+				'params'=>array(':type'=>$type),
+				'order'=>'position',
+			));
+		foreach($models as $model)
+			self::$_items[$type][$model->id]=$model->name;
 	}
 }
